214ee9700be97cca6cad3e0a5355a37421a78195
Ibrahim Cesar <ibracnb@amazon.com>
2023-11-14 13:36:05 -0300

    fix: twitter removed since no longer exists and header menu fixed

--- src/components/icons/index.js
+++ src/components/icons/index.js
@@ -1,6 +1,5 @@
 import IconSun from "./IconSun.astro";
 import IconGithub from "./IconGithub.astro";
-import IconTwitter from "./IconTwitter.astro";
 import IconArrowRight from "./IconArrowRight.astro";
 import IconMenu from "./IconMenu.astro";
 import IconArrowDownRight from "./IconArrowDownRight.astro";
@@ -9,7 +8,6 @@ import IconRSS from "./IconRSS.astro";
 export {
   IconSun,
   IconGithub,
-  IconTwitter,
   IconArrowRight,
   IconMenu,
   IconArrowDownRight,
